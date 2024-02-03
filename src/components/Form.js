59fcd60f925f7c4ffeff0729314d599ceba450f8
Brandon Lowe <BLoweCLE@gmail.com>
2024-02-03 13:19:20 -0600

    form responsive styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,10 @@ import React from 'react'
 import '../css/form.css';
 
 export default function Form() {
-    window.scrollTo(0, 0);
+    setTimeout(() => {
+        window.scrollTo(0, 0);
+    }, 100);
+
   return (
     <div className="form-container">
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe-6HNBjq10lJFHdqG4WhVT70xy48CVAEms6J0LybSZ3Bu2QQ/viewform?vc=0&c=0&w=1&flr=0" className="form" frameBorder="0">Loading…</iframe>
