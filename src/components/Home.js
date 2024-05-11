413bdd28d651f1f70b2b277a9c820ccf80779d39
Brandon Lowe <BLoweCLE@gmail.com>
2024-05-10 21:07:13 -0400

    button update

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
       </div>
       <div className="main-navigation">
             <div className="grant-banner">
-              Thanks to a generous support from Clare County Arts Council, Glór, Cois na hAbhna and the Fleadh Núa we are now able to adjust pricing to encourage more local participants.  We're excited that the new fee for 2024 will be €300.  Register today!
+              Thanks to a generous support from Clare County Arts Council, Glór, Cois na hAbhna and the Fleadh Núa we are now able to adjust pricing to encourage more local participants.  We're excited that the new fee for 2024 will be €300.  <Link to="/register"><button className="main-navigation-button">Register today!</button></Link>
             </div>
         <div className="main-navigation-container">
           <div className="date-wrapper">
